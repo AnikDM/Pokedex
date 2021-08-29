27a299666fa96690e4153cf7d5691fe105c507ac
AnikDM <86837164+AnikDM@users.noreply.github.com>
2021-08-30 01:34:36 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const pokedex = document.getElementById("pokedex");
-
+const mybutton = document.getElementById("btn");
 const fetchPokemon = () => {
     const promises = [];
-    for (let i = 1; i <= 895; i++) {
+    for (let i = 1; i <= 898; i++) {
         const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
         promises.push(fetch(url).then((res) => res.json()));
     }
@@ -46,3 +46,21 @@ const displayPokemon = (pokemon) => {
 //     console.log(`${pokemon.name}`)
 // };
 fetchPokemon();
+
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0;
+}
